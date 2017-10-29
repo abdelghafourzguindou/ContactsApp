4f58069b97ba04ecc97e84a7c65548efd04ec446
zguindou <zguindou@gmail.com>
2017-10-29 01:23:34 +0000

    finished v1.0

--- src/main/java/org/sid/web/ContactRestService.java
+++ src/main/java/org/sid/web/ContactRestService.java
@@ -1,7 +1,62 @@
 package org.sid.web;
 
-public class ContactRestService {
+import java.util.List;
 
+import org.sid.dao.ContactRepository;
+import org.sid.entity.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class ContactRestService {
+	
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	@RequestMapping(value = "/contacts", method = RequestMethod.GET)
+	public List<Contact> getContacts() {
+		return contactRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/contacts/{id}", method = RequestMethod.GET)
+	public Contact getContactByID(@PathVariable Long id) {
+		return contactRepository.findOne(id);
+	}
+	
+	@RequestMapping(value = "/searchContacts", method = RequestMethod.GET)
+	public Page<Contact> searchContact(
+			@RequestParam(name="mc",   defaultValue="")     String mc, 
+			@RequestParam(name="page", defaultValue="0") 	int page, 
+			@RequestParam(name="size", defaultValue="5") 	int size) {
+		return contactRepository.searchContact("%"+mc+"%", new PageRequest(page, size));
+	}
+	
+	@RequestMapping(value = "/contacts", method = RequestMethod.POST)
+	public Contact saveContact(@RequestBody Contact c) {
+		return contactRepository.save(c);
+	}
+	
+	@RequestMapping(value = "/contacts/{id}", method = RequestMethod.DELETE)
+	public boolean deleteContact(@PathVariable Long id) {
+		contactRepository.delete(id);
+		return true;
+	}
+	
+	@RequestMapping(value = "/contacts/{id}", method = RequestMethod.PUT)
+	public Contact updateContact(@PathVariable Long id, @RequestBody Contact c) {
+		c.setId(id);
+		return contactRepository.save(c);
+	}
+	
 	public ContactRestService() {
 		// TODO Auto-generated constructor stub
 	}
